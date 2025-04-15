dc40176e7ad40adcb67a1e15f9dbb1adb28aa9cb
Ramona <rare2400@student.miun.com>
2025-04-15 13:15:57 +0200

    lägger till felmeddelanden vid error

--- install.js
+++ install.js
@@ -6,20 +6,34 @@
 const sqlite3 = require('sqlite3').verbose();
 
 //skapar databasen
-const db = new sqlite3.Database("./db/cv.db");
+const db = new sqlite3.Database("./db/cv.db", (err) => {
+        //felmeddelande vid fel när databasen skapas
+        if (err) {
+                throw err;
+        }
+});
 
 //skapar tabell (id, coursecode, coursename, syllabus, progression)
 db.serialize(() => {
-    db.run(`DROP TABLE IF EXISTS courses;`)
+        //tar bort tabell om den finns
+        db.run(`DROP TABLE IF EXISTS courses;`)
 
-    db.run(`CREATE TABLE IF NOT EXISTS courses (
+        //skapar tabellen
+        db.run(`CREATE TABLE IF NOT EXISTS courses (
                 CourseID INTEGER PRIMARY KEY AUTOINCREMENT,
                 CourseCode TEXT NOT NULL,
                 CourseName TEXT NOT NULL,
                 Syllabus TEXT,
                 Progression TEXT, 
                 Created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        );`);
+        );`
+                //felmeddelande vid fel när tabellen skapas
+                , (err) => {
+                        if (err) {
+                                console.error("Fel när tabell skapas:", err.message)
+                        }
+                });
 });
 
+//stänger databasen
 db.close();
